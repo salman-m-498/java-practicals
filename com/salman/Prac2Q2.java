ed0742f9bf4b8e38e0ad2341281a4a97710c705d
Moosa Salmaan <141954099+salman-m-498@users.noreply.github.com>
2025-07-04 17:16:03 +0000

    added Prac2Q2 - Completed

---
+++ com/salman/Prac2Q2.java
@@ -0,0 +1,77 @@
+package com.salman;
+
+import java.util.Scanner;
+
+public class Prac2Q2 
+{
+public static double sqrt(int num)
+{
+    double REQUIRED_PRECISION = 1e-8;
+        
+        // Handle special cases
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        
+        if (num > 1) {
+            double min = 1;
+            double max = num;
+            double mid;
+            double midSquare;
+            double precision;
+            
+            // Loop until we reach required precision
+            do 
+            {
+                mid = (max + min) / 2;
+                midSquare = mid * mid;
+                precision = (max - min)/min;
+                
+                // Adjust search bounds based on whether mid^2 is too big or too small
+                if (midSquare > num) 
+                {
+                    max = mid;  // Search in lower half
+                } else 
+                {
+                    min = mid;  // Search in upper half
+                }
+                
+            } while (precision > REQUIRED_PRECISION);
+            
+            return mid;
+        }
+        // For numbers between 0 and 1
+        else 
+        {
+            double min = 0;
+            double max = 1;
+            double mid;
+            double midSquare;
+            double precision;
+            
+            do 
+            {
+                mid = (max + min) / 2;
+                midSquare = mid * mid;
+                precision = (max - min)/min;
+                
+                if (midSquare > num) {
+                    max = mid;
+                } else {
+                    min = mid;
+                }
+                
+            } while (precision > REQUIRED_PRECISION);
+            
+            return mid;
+        }
+}
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num = in.nextInt();
+        System.out.println(sqrt(num));
+        in.close();
+    }
+}
